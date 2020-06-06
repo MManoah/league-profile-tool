cecf8380ce3b2370b37b823516cf3279a1e6461c
MManoah <47468428+MManoah@users.noreply.github.com>
2020-06-06 04:35:18 -0400

    Version

--- src/javascript/home.js
+++ src/javascript/home.js
@@ -1,8 +1,14 @@
 var homeIcons = document.querySelectorAll(".icon");
-
+var newVersion = document.querySelector("#newVersion");
 mouseDown(0, "https://github.com/MManoah");
 mouseDown(1, "https://www.youtube.com/channel/UCd5nD5lMlHe55UW4iAlJGJA");
-
+async function load() {
+  let url =
+    "https://raw.githubusercontent.com/MManoah/league-profile-tool/master/version.json";
+  let obj = await (await fetch(url)).json();
+  newVersion.innerHTML = `Newest Version: ${obj["version"]}`;
+}
+load();
 for (var i = 0; i < homeIcons.length; i++) {
   homeIcons[i].addEventListener("mouseover", function () {
     this.classList.add("gitIconToggle");
