690f196986e024629acadda83db13156c0d26c43
MManoah <47468428+MManoah@users.noreply.github.com>
2020-07-06 19:06:31 -0400

    fixed issue
    fixed an issue where the window was not draggable

--- src/javascript/background.js
+++ src/javascript/background.js
@@ -37,7 +37,7 @@ load().then(function () {
         });
         node.addEventListener("mousedown", function () {
           load().then(function (skins) {
-            champions.innerHTML = "<h1>Select a Skin</h1>";
+            champions.innerHTML = "<h1 style=\"margin-top: 5px\">Select a Skin</h1>";
             for (var i = 0; i < skins.length; i++) {
               var node = document.createElement("IMG");
               node.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${property}_${skins[i]["num"]}.jpg`;
