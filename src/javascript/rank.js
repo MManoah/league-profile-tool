486a75f6a68fbb5a7e8a8bdb5c9e09a55ee2dad7
MManoah <47468428+MManoah@users.noreply.github.com>
2020-07-09 13:04:21 -0400

    Chat Rank UI update

--- src/javascript/rank.js
+++ src/javascript/rank.js
@@ -1,7 +1,9 @@
-var setRank = document.querySelector("#setRank");
-var tier = document.querySelector("#tier");
-var division = document.querySelector("#division");
-var queue = document.querySelector("#queue");
+var queue = document.querySelectorAll(".queue");
+var rankMain = document.querySelector(".rankMain");
+var ranks = ["IRON", "BRONZE", "SILVER", "GOLD", "PLATINUM", "DIAMOND"];
+var divisions = ["IV", "III", "II", "I"];
+var divValue = 0;
+var value = 0;
 var rankInfo = {
   lol: {
     rankedLeagueQueue: "",
@@ -13,35 +15,62 @@ var optionsCopy = Object.assign({}, options);
 optionsCopy["url"] = `${optionsCopy["url"]}/lol-chat/v1/me`; // End point to set chat rank
 optionsCopy["method"] = "PUT";
 
-tier.addEventListener("change", function () {
-  if (
-    this.value === "MASTER" ||
-    this.value === "GRANDMASTER" ||
-    this.value === "CHALLENGER"
-  ) {
-    division.disabled = true;
-  } else {
-    division.disabled = false;
-  }
-});
-
-setRank.addEventListener("mousedown", function () {
-  rankInfo["lol"]["rankedLeagueTier"] = tier.value;
-  rankInfo["lol"]["rankedLeagueDivision"] = division.value;
-  if (queue.value === "SOLO") {
-    rankInfo["lol"]["rankedLeagueQueue"] = "RANKED_SOLO_5x5";
-  } else {
-    rankInfo["lol"]["rankedLeagueQueue"] = "RANKED_TFT";
-  }
-  optionsCopy["body"] = JSON.stringify(rankInfo);
-  run();
-});
-setRank.addEventListener("mouseover", function () {
-  this.classList.add("rankOver");
-});
-setRank.addEventListener("mouseleave", function () {
-  this.classList.remove("rankOver");
-});
+for (var i = 0; i < queue.length; i++) {
+  queue[i].addEventListener("mouseover", function () {
+    this.classList.add("queueButtonToggle");
+  });
+  queue[i].addEventListener("mouseleave", function () {
+    this.classList.remove("queueButtonToggle");
+  });
+  queue[i].addEventListener("mousedown", function () {
+    rankInfo["lol"]["rankedLeagueQueue"] = this.id;
+    rankMain.innerHTML = '<h1 style="margin-top: 5px">Select A Rank</h1>';
+    for (var i = 1; i < 28; i++) {
+      var node = document.createElement("IMG");
+      node.src = `../src/images/Ranked Emblems/${i}.png`;
+      node.width = "150";
+      node.classList.add("rankedEmblem");
+      node.addEventListener("mouseover", function () {
+        this.classList.add("queueButtonToggle");
+      });
+      node.addEventListener("mouseleave", function () {
+        this.classList.remove("queueButtonToggle");
+      });
+      if (i % 4 != 0 && i != 25 && i != 26 && i != 27) {
+        node.alt = `${ranks[value]}`;
+        node.id = divisions[divValue];
+        divValue++;
+      } else if (i % 4 == 0 && i != 25 && i != 26 && i != 27) {
+        node.alt = `${ranks[value]}`;
+        node.id = divisions[divValue];
+        divValue = 0;
+        value++;
+      } else {
+        this.id = "";
+        switch (i) {
+          case 25:
+            node.alt = "MASTER";
+            break;
+          case 26:
+            node.alt = "GRANDMASTER";
+            break;
+          case 27:
+            node.alt = "CHALLENGER";
+            break;
+          default:
+            break;
+        }
+      }
+      node.addEventListener("mousedown", function () {
+        rankInfo["lol"]["rankedLeagueTier"] = this.alt;
+        rankInfo["lol"]["rankedLeagueDivision"] = this.id;
+        optionsCopy["body"] = JSON.stringify(rankInfo);
+        run();
+      });
+      rankMain.appendChild(node);
+    }
+  });
+}
 function callback(error, response) {
   var dialogOptions = {};
   if (!error && response.statusCode === 201) {
