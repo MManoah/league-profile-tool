4eed3030134525bdc23c1661860c694cf18a78d5
MManoah <47468428+MManoah@users.noreply.github.com>
2020-07-10 18:14:47 -0400

    Create aram.js

---
+++ src/javascript/aram.js
@@ -0,0 +1,43 @@
+var boost = document.getElementById("boost");
+var optionsCopy = Object.assign({}, options);
+optionsCopy[
+  "url"
+] = `${optionsCopy["url"]}/lol-champ-select/v1/team-boost/purchase`;
+optionsCopy["method"] = "POST";
+
+boost.addEventListener("mousedown", function () {
+  var dialogOptions = {
+    type: "question",
+    title: "Confirmation",
+    message:
+      "Are you sure you want to continue? \n(Make sure you don't have enough RP for a boost)",
+    buttons: ["Yes", "No"],
+  };
+
+  dialog.showMessageBox(dialogOptions).then((response) => {
+    if (response.response === 0) run();
+  });
+});
+
+function callback(error, response) {
+  var dialogOptions = {};
+  console.log(response.statusCode);
+  if (!error && response.statusCode === 204) {
+    dialogOptions = {
+      type: "info",
+      title: "Success",
+      message: `Success`,
+    };
+  } else {
+    dialogOptions = {
+      type: "error",
+      title: "Error",
+      message: "There was an error",
+    };
+  }
+  dialog.showMessageBox(dialogOptions);
+}
+
+function run() {
+  request(optionsCopy, callback);
+}
