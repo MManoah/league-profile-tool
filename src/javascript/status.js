c5ccefa820e18d8a334df3077ea9d119e89f0839
Paradax-Elite <69823828+Paradax-Elite@users.noreply.github.com>
2020-08-23 22:27:56 -0400

    Update status.js

--- src/javascript/status.js
+++ src/javascript/status.js
@@ -6,5 +6,5 @@ var message = {
 
 setStatus.addEventListener("mousedown", function () {
   message["statusMessage"] = text.value;
-  makeRequest("PUT", message, "/lol-chat/v1/me");
+  LeagueClient.makeRequest("PUT", message, "/lol-chat/v1/me");
 });
